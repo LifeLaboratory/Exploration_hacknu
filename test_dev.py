a3a78ef63796c0aed7468994b319a2acc6741578
raldenprog <krylosov.andrew@gmail.com>
2021-03-20 17:46:39 +0700

    тест

---
+++ test_dev.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from app.users.processor import Processor
+
+
+class TestAitu(TestCase):
+
+    def test_dev(self):
+        Processor().login({
+            'id': '1',
+            'avatar': 'https://avatars.githubusercontent.com/u/23695322?s=400&v=4',
+            'description': 'asdaasdsd',
+            'name': 'name',
+            'lastname': 'lastname',
+            'avatarThumb': 'avatarThumb',
+            'phone': 'phone',
+            'latitude': 123.123,
+            'longitude': 123.123,
+        })
+
+    def test_dev_2(self):
+        Processor().swipe({
+            'id_first': '1',
+            'id_second': '2',
+            'status': True,
+        })
