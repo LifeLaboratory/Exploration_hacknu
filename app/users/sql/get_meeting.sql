5e080442cc1f3ed557e9bc4fdda4a884ac44a180
raldenprog <krylosov.andrew@gmail.com>
2021-03-20 19:53:03 +0700

    получить встречу

--- app/users/sql/get_meeting.sql
+++ app/users/sql/get_meeting.sql
@@ -2,13 +2,13 @@ select
 
 json_build_object(
     'user', json_build_object(
-                    'id', u1.id,
-                    'avatar', u1.avatar,
-                    'description', u1.description,
-                    'name', u1.name,
-                    'lastname', u1.lastname,
-                    'avatarThumb', u1."avatarThumb",
-                    'phone', u1.phone
+                    'id', coalesce(u1.id, u2.id),
+                    'avatar', coalesce(u1.avatar, u2.avatar),
+                    'description', coalesce(u1.description, u2.description),
+                    'name', coalesce(u1.name, u2.name),
+                    'lastname', coalesce(u1.lastname, u2.lastname),
+                    'avatarThumb', coalesce(u1."avatarThumb", u1."avatarThumb"),
+                    'phone', coalesce(u1.phone, u2.phone)
     ),
     'place', json_build_object(
                     'id_place', p.id_place,
@@ -23,34 +23,6 @@ from
 
 meetings m
 left join places p on p.id_place = m.id_place
-left join users u1 on u1.id = m.id_first
-where m.id_second = {id}::text
-
-union all
-select
-
-json_build_object(
-    'user', json_build_object(
-                    'id', u2.id,
-                    'avatar', u2.avatar,
-                    'description', u2.description,
-                    'name', u2.name,
-                    'lastname', u2.lastname,
-                    'avatarThumb', u2."avatarThumb",
-                    'phone', u2.phone
-    ),
-    'place', json_build_object(
-                    'id_place', p.id_place,
-                    'link', p.link,
-                    'latitude', p.latitude,
-                    'longitude', p.longitude
-    ),
-    'datetime', m.datetime
-) meeting_info
-
-from
-
-meetings m
-left join places p on p.id_place = m.id_place
-left join users u2 on u2.id = m.id_second
-where m.id_first = {id}::text
+left join users u1 on u1.id = m.id_first and u1.id != {id}::text
+left join users u2 on u2.id = m.id_second and u2.id != {id}::text
+where m.id_second = {id}::text or m.id_first = {id}::text
